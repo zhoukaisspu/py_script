6a42bbf75593965da28adf50b17b0bb986e722f0
kaizhou <kaizhou@oymotion.com>
2018-03-16 17:54:13 +0800

    Add clean.py to clean vs2017 temporary file

---
+++ clean.py
@@ -0,0 +1,64 @@
+import os
+
+__author__ = 'KaiZhou'
+
+suffix = ('.txt',
+        '.ilk',
+        '.pdb',
+        '.iobj',
+        '.ipdb',
+        '.exe',
+        '.user',
+        '.log',
+        '.pch',
+        '.tlog',
+        '.db',
+        '.opendb',
+        '.ipch',
+        '.suo',
+        '.obj',
+        '.lastbuildstate',
+        '.idb',
+        '')
+
+def _getDirs(path='.'):
+    dirs = os.listdir(path)
+    index = 0
+    for i in dirs:
+        dirs[index] = os.path.join(path,i)
+        index = index + 1
+    return dirs
+
+def _containExt(ext):
+    for i in suffix:
+        if i == ext:
+            return True
+    return False
+
+def _RemoveTemporaryFile(path='.'):
+    dirs = _getDirs(path)
+    for i in dirs:
+        if os.path.isdir(i):
+            _RemoveTemporaryFile(i)
+        else:
+           if _containExt(os.path.splitext(i)[1]):
+                os.remove(i)
+                print("start remove file:%s" % i)
+
+def _RemoveEmptyDirectory(path='.'):
+    dirs = _getDirs(path)
+    for i in dirs:
+        if os.path.isdir(i):
+            if len(os.listdir(i)) == 0:
+                os.removedirs(i)
+                print('start remove directory:%s'% i)
+            else:
+                _RemoveEmptyDirectory(i)
+
+def CleanBuildTree(path='.'):
+    _RemoveTemporaryFile(path)
+    _RemoveEmptyDirectory(path)
+
+
+if __name__=='__main__':
+    CleanBuildTree()
